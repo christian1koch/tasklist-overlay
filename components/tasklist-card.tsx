52865ea6b9dc083fa16a1a2fe4a48af47b83a4f7
Christian Koch Echeverria <s0566264@htw-berlin.de>
2026-03-08 14:53:25 +0100

    Update height

--- components/tasklist-card.tsx
+++ components/tasklist-card.tsx
@@ -1,25 +1,25 @@
 import { TaskList } from "./task-list";
 
 type Task = {
-  id: string;
-  title: string;
-  completed: boolean;
-  completedAt: Date | null;
-  createdAt: Date;
+    id: string;
+    title: string;
+    completed: boolean;
+    completedAt: Date | null;
+    createdAt: Date;
 };
 
 type Props = {
-  ownerName: string;
-  tasks: Task[];
+    ownerName: string;
+    tasks: Task[];
 };
 
 export function TasklistCard({ ownerName, tasks }: Props) {
-  return (
-    <div className="absolute right-0 bottom-0 m-10 w-full max-w-xs rounded-xl border bg-black/90 p-4 shadow-md">
-      <h1 className="text-primary mb-4 text-center text-2xl font-bold">
-        {ownerName}&apos;s Tasks
-      </h1>
-      <TaskList tasks={tasks} />
-    </div>
-  );
+    return (
+        <div className="absolute right-0 bottom-0 m-10 min-h-130 w-full max-w-xs rounded-xl border bg-black/90 p-4 shadow-md">
+            <h1 className="text-primary mb-4 text-center text-2xl font-bold">
+                {ownerName}&apos;s Tasks
+            </h1>
+            <TaskList tasks={tasks} />
+        </div>
+    );
 }
